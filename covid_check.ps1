fb5b9c49739f1faed742bb707407df8de339092e
pacheung <info@example.org>
2020-12-09 21:50:43 +0800

    removed useless settings

--- covid_check.ps1
+++ covid_check.ps1
@@ -1,5 +1,5 @@
 ﻿cd C:\users\ckska\repo\covid_check
-Invoke-WebRequest -uri http://www.chp.gov.hk/files/misc/enhanced_sur_covid_19_eng.csv -OutFile C:\users\ckska\repo\covid_check\enhanced_sur_covid_19_eng.csv
+Invoke-WebRequest -uri http://www.chp.gov.hk/files/misc/enhanced_sur_covid_19_eng.csv -OutFile enhanced_sur_covid_19_eng.csv
 $CSV = (gc enhanced_sur_covid_19_eng.csv)|ConvertFrom-Csv
 $CSV| format-table|Measure-Object -line
 
