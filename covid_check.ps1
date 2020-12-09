551f64b1657fc185b01cc17e23916bce30704066
pacheung <info@example.org>
2020-12-09 21:47:26 +0800

    line counter

--- covid_check.ps1
+++ covid_check.ps1
@@ -4,5 +4,6 @@
 cd C:\users\ckska\repo\covid_check
 Invoke-WebRequest -uri http://www.chp.gov.hk/files/misc/enhanced_sur_covid_19_eng.csv -OutFile C:\users\ckska\repo\covid_check\enhanced_sur_covid_19_eng.csv
 $CSV = (gc enhanced_sur_covid_19_eng.csv)|ConvertFrom-Csv
+$CSV| format-table|Measure-Object -line
 
 $CSV[0]
