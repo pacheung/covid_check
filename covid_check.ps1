4a68ddcd9d3a53d438b99e6421f449e2b4f79b06
pacheung <info@example.org>
2020-12-09 09:57:43 +0800

    fixed minor

--- covid_check.ps1
+++ covid_check.ps1
@@ -1,6 +1,8 @@
-﻿cd C:\users\ckska\repo\covid_check
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
+[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
+
+cd C:\users\ckska\repo\covid_check
 Invoke-WebRequest -uri http://www.chp.gov.hk/files/misc/enhanced_sur_covid_19_eng.csv -OutFile C:\users\ckska\repo\covid_check\enhanced_sur_covid_19_eng.csv
 $CSV = (gc enhanced_sur_covid_19_eng.csv)|ConvertFrom-Csv
-$CSV2 = 
 
 $CSV[0]
