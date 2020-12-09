c757a1562ed983da2a02003ff4c97c577fb72bfc
pacheung <info@example.org>
2020-12-09 21:49:41 +0800

    removed useless settings

--- covid_check.ps1
+++ covid_check.ps1
@@ -1,7 +1,4 @@
-﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls, [Net.SecurityProtocolType]::Tls11, [Net.SecurityProtocolType]::Tls12, [Net.SecurityProtocolType]::Ssl3
-[Net.ServicePointManager]::SecurityProtocol = "Tls, Tls11, Tls12, Ssl3"
-
-cd C:\users\ckska\repo\covid_check
+﻿cd C:\users\ckska\repo\covid_check
 Invoke-WebRequest -uri http://www.chp.gov.hk/files/misc/enhanced_sur_covid_19_eng.csv -OutFile C:\users\ckska\repo\covid_check\enhanced_sur_covid_19_eng.csv
 $CSV = (gc enhanced_sur_covid_19_eng.csv)|ConvertFrom-Csv
 $CSV| format-table|Measure-Object -line
