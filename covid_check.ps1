7b635794a735e7f9903451bda835a5984f277475
pacheung <info@example.org>
2020-12-10 19:39:25 +0800

    Renamed Field Hospitalised as Status;Replace -Match with -EQ

--- covid_check.ps1
+++ covid_check.ps1
@@ -9,7 +9,7 @@ $CSV2=$CSV|select @{Name="Case";Expression = {[int]$_."Case no."}},
     Gender,
     @{Name="Age";Expression = {[int]$_.Age}},
     @{Name="AG";Expression = {[int][Math]::Floor($_.Age/10)*10}},
-    @{Name="Hospitalised";Expression = {$_."Hospitalised/Discharged/Deceased"}},
+    @{Name="Status";Expression = {$_."Hospitalised/Discharged/Deceased"}},
     @{Name="Residency";Expression = {$_."HK/Non-HK resident"}},
     @{Name="Class";Expression = {$_."Case classification*"}},
     @{Name="Confirmed";Expression = {$_."Confirmed/probable"}}
@@ -21,53 +21,52 @@ $TOT=($CSV2).count
 $CSV2|select -Last 10|ft
 
 "Last 7 Days:",
-    (($CSV2|where Class -match "Imported case"|where "D_Report" -GE (Get-Date).AddDays(-7)).count),"(Imported) /",
+    (($CSV2|where Class -EQ "Imported case"|where "D_Report" -GE (Get-Date).AddDays(-7)).count),"(Imported) /",
     (($CSV2|where "D_Report" -GE (Get-Date).AddDays(-7)).count) -join ' '
 "Last 2 Days:",
-    (($CSV2|where Class -match "Imported case"|where "D_Report" -GE (Get-Date).AddDays(-2)).count),"(Imported) /",
+    (($CSV2|where Class -EQ "Imported case"|where "D_Report" -GE (Get-Date).AddDays(-2)).count),"(Imported) /",
     (($CSV2|where "D_Report" -GE (Get-Date).AddDays(-2)).count) -join ' '
 "Hospitalized:",
-    (($CSV2|where Class -match "Imported case"|where "Hospitalised" -Match "Hospitalised").count),"(Imported) /",
-    (($CSV2|where "Hospitalised" -Match "Hospitalised").count) -join ' '
+    (($CSV2|where Class -EQ "Imported case"|where "Status" -EQ "Hospitalised").count),"(Imported) /",
+    (($CSV2|where "Status" -EQ "Hospitalised").count) -join ' '
 "Status: ",
-    (($CSV2|where "Hospitalised" -Match "To be provided").count),"(To be provided)| ",
-    (($CSV2|where "Hospitalised" -Match "Hospitalised").count),"(Hospitalised)| ",
-    (($CSV2|where "Hospitalised" -Match "Discharged").count),"(Discharged)| ",
-    (($CSV2|where "Hospitalised" -Match "Deceased").count),"(Deceased)" -join ' '
+    (($CSV2|where "Status" -EQ "To be provided").count),"(To be provided) | ",
+    (($CSV2|where "Status" -EQ "Hospitalised").count),"(Hospitalised) | ",
+    (($CSV2|where "Status" -EQ "Discharged").count),"(Discharged) | ",
+    (($CSV2|where "Status" -EQ "Deceased").count),"(Deceased)" -join ' '
 "Gender: ",
-    (($CSV2|where Gender -match "M").count),"(M) | ",
-    (($CSV2|where Gender -match "F").count),"(F)" -join ' '
+    (($CSV2|where Gender -EQ "M").count),"(M) | ",
+    (($CSV2|where Gender -EQ "F").count),"(F)" -join ' '
 "Age Group: ",
-    (($CSV2|where AG -eq 0).count),"(0s)| ",
-    (($CSV2|where AG -eq 10).count),"(10s)| ",
-    (($CSV2|where AG -eq 20).count),"(20s)| ",
-    (($CSV2|where AG -eq 30).count),"(30s)| ",
-    (($CSV2|where AG -eq 40).count),"(40s)| ",
-    (($CSV2|where AG -eq 50).count),"(50s)| ",
-    (($CSV2|where AG -eq 60).count),"(60s)| ",
-    (($CSV2|where AG -eq 70).count),"(70s)| ",
-    (($CSV2|where AG -eq 80).count),"(80s)| ",
-    (($CSV2|where AG -eq 90).count),"(90s)| ",
-    (($CSV2|where AG -eq 100).count),"(100s)" -join ' '
+    (($CSV2|where AG -EQ 0).count),"(0s)| ",
+    (($CSV2|where AG -EQ 10).count),"(10s)| ",
+    (($CSV2|where AG -EQ 20).count),"(20s)| ",
+    (($CSV2|where AG -EQ 30).count),"(30s)| ",
+    (($CSV2|where AG -EQ 40).count),"(40s)| ",
+    (($CSV2|where AG -EQ 50).count),"(50s)| ",
+    (($CSV2|where AG -EQ 60).count),"(60s)| ",
+    (($CSV2|where AG -EQ 70).count),"(70s)| ",
+    (($CSV2|where AG -EQ 80).count),"(80s)| ",
+    (($CSV2|where AG -EQ 90).count),"(90s)| ",
+    (($CSV2|where AG -EQ 100).count),"(100s)" -join ' '
 "Age Group: ",
-    (($CSV2|where AG -eq 0).count/$TOT).tostring("P"),"(0s)| ",
-    (($CSV2|where AG -eq 10).count/$TOT).tostring("P"),"(10s)| ",
-    (($CSV2|where AG -eq 20).count/$TOT).tostring("P"),"(20s)| ",
-    (($CSV2|where AG -eq 30).count/$TOT).tostring("P"),"(30s)| ",
-    (($CSV2|where AG -eq 40).count/$TOT).tostring("P"),"(40s)| ",
-    (($CSV2|where AG -eq 50).count/$TOT).tostring("P"),"(50s)| ",
-    (($CSV2|where AG -eq 60).count/$TOT).tostring("P"),"(60s)| ",
-    (($CSV2|where AG -eq 70).count/$TOT).tostring("P"),"(70s)| ",
-    (($CSV2|where AG -eq 80).count/$TOT).tostring("P"),"(80s)| ",
-    (($CSV2|where AG -eq 90).count/$TOT).tostring("P"),"(90s)| ",
-    (($CSV2|where AG -eq 100).count/$TOT).tostring("P"),"(100s)" -join ' '
+    (($CSV2|where AG -EQ 0).count/$TOT).tostring("P"),"(0s)| ",
+    (($CSV2|where AG -EQ 10).count/$TOT).tostring("P"),"(10s)| ",
+    (($CSV2|where AG -EQ 20).count/$TOT).tostring("P"),"(20s)| ",
+    (($CSV2|where AG -EQ 30).count/$TOT).tostring("P"),"(30s)| ",
+    (($CSV2|where AG -EQ 40).count/$TOT).tostring("P"),"(40s)| ",
+    (($CSV2|where AG -EQ 50).count/$TOT).tostring("P"),"(50s)| ",
+    (($CSV2|where AG -EQ 60).count/$TOT).tostring("P"),"(60s)| ",
+    (($CSV2|where AG -EQ 70).count/$TOT).tostring("P"),"(70s)| ",
+    (($CSV2|where AG -EQ 80).count/$TOT).tostring("P"),"(80s)| ",
+    (($CSV2|where AG -EQ 90).count/$TOT).tostring("P"),"(90s)| ",
+    (($CSV2|where AG -EQ 100).count/$TOT).tostring("P"),"(100s)" -join ' '
 "Residency: ",
-    (($CSV2|where Residency -match "HK resident").count),"(Local) | ",
-    (($CSV2|where Residency -match "Non-HK resident").count),"(Non-HK)" -join ' '
+    (($CSV2|where Residency -EQ "HK resident").count),"(Local) | ",
+    (($CSV2|where Residency -EQ "Non-HK resident").count),"(Non-HK)" -join ' '
 "Class: ",
-    (($CSV2|where Class -match "Imported case").count),"(Imported) | ",
-    (($CSV2|where Class -match "Local case").count),"(Local) | ",
-    (($CSV2|where Class -match "Epidemiologically linked with local case").count),"(Linked)" -join ' '
-"Total: ",
-    (($CSV2|where Class -match "Imported case").count),"(Imported) /",
-    (($CSV2).count) -join ' '
+    (($CSV2|where Class -EQ "Imported case").count),"(Imported) | ",
+    (($CSV2|where Class -EQ "Local case").count),"(Local) | ",
+    (($CSV2|where Class -EQ "Epidemiologically linked with local case").count),"(Linked) /",
+    $TOT -join ' '
+
