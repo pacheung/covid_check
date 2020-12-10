0dd4408fba34f3675c5801f8175db894f29c34b9
pacheung <info@example.org>
2020-12-10 23:23:33 +0800

    Use for loop to handle age group, add a chart illustration

--- covid_check.ps1
+++ covid_check.ps1
@@ -37,30 +37,18 @@ $CSV2|select -Last 10|ft
 "Gender: ",
     (($CSV2|where Gender -EQ "M").count),"(M) | ",
     (($CSV2|where Gender -EQ "F").count),"(F)" -join ' '
-"Age Group: ",
-    (($CSV2|where AG -EQ 0).count),"(0s)| ",
-    (($CSV2|where AG -EQ 10).count),"(10s)| ",
-    (($CSV2|where AG -EQ 20).count),"(20s)| ",
-    (($CSV2|where AG -EQ 30).count),"(30s)| ",
-    (($CSV2|where AG -EQ 40).count),"(40s)| ",
-    (($CSV2|where AG -EQ 50).count),"(50s)| ",
-    (($CSV2|where AG -EQ 60).count),"(60s)| ",
-    (($CSV2|where AG -EQ 70).count),"(70s)| ",
-    (($CSV2|where AG -EQ 80).count),"(80s)| ",
-    (($CSV2|where AG -EQ 90).count),"(90s)| ",
-    (($CSV2|where AG -EQ 100).count),"(100s)" -join ' '
-"Age Group: ",
-    (($CSV2|where AG -EQ 0).count/$TOT).tostring("P"),"(0s)| ",
-    (($CSV2|where AG -EQ 10).count/$TOT).tostring("P"),"(10s)| ",
-    (($CSV2|where AG -EQ 20).count/$TOT).tostring("P"),"(20s)| ",
-    (($CSV2|where AG -EQ 30).count/$TOT).tostring("P"),"(30s)| ",
-    (($CSV2|where AG -EQ 40).count/$TOT).tostring("P"),"(40s)| ",
-    (($CSV2|where AG -EQ 50).count/$TOT).tostring("P"),"(50s)| ",
-    (($CSV2|where AG -EQ 60).count/$TOT).tostring("P"),"(60s)| ",
-    (($CSV2|where AG -EQ 70).count/$TOT).tostring("P"),"(70s)| ",
-    (($CSV2|where AG -EQ 80).count/$TOT).tostring("P"),"(80s)| ",
-    (($CSV2|where AG -EQ 90).count/$TOT).tostring("P"),"(90s)| ",
-    (($CSV2|where AG -EQ 100).count/$TOT).tostring("P"),"(100s)" -join ' '
+$AG_Str="Age Group: "
+for ($num = 0 ; $num -le 100 ; $num+=10)
+{
+	$AG_Str += (($CSV2|where AG -EQ $num).count)," (",$num,"s)| " -join ''
+}
+$AG_Str
+$AGP_Str="Age Group: "
+for ($num = 0 ; $num -le 100 ; $num+=10)
+{
+	$AGP_Str += (($CSV2|where AG -EQ $num).count/$TOT).tostring("P")," (",$num,"s) | " -join ''
+}
+$AGP_Str
 "Residency: ",
     (($CSV2|where Residency -EQ "HK resident").count),"(Local) | ",
     (($CSV2|where Residency -EQ "Non-HK resident").count),"(Non-HK)" -join ' '
@@ -70,3 +58,50 @@ $CSV2|select -Last 10|ft
     (($CSV2|where Class -EQ "Epidemiologically linked with local case").count),"(Linked) /",
     $TOT -join ' '
 
+
+$D_30=$CSV2|where "D_Report" -LT (Get-Date).AddDays(-30)
+$T30=$D_30.count
+"D-30 Status: ",
+    (($D_30|where "Status" -EQ "To be provided").count/$T30).tostring("P"),"(To be provided) | ",
+    (($D_30|where "Status" -EQ "Hospitalised").count/$T30).tostring("P"),"(Hospitalised) | ",
+    (($D_30|where "Status" -EQ "Discharged").count/$T30).tostring("P"),"(Discharged) | ",
+    (($D_30|where "Status" -EQ "Deceased").count/$T30).tostring("P"),"(Deceased) / ",
+    $T30 -join ' '
+
+$D14=$CSV2|where "D_Report" -GE (Get-Date).AddDays(-14)
+$T14=$D14.count
+$D14A_Str="D14 AgeGroup: "
+$D14A_HEADER=@()
+$D14A_DATA=@()
+for ($num = 0 ; $num -le 100 ; $num+=10)
+{
+	$D14A_Str += (($D14|where AG -EQ $num).count/$T14).tostring("P")," (",$num,"s)| " -join ''
+	$D14A_HEADER += $num,"s" -join ""
+	$D14A_DATA += (($D14|where AG -EQ $num).count)
+}
+$D14A_Str += $T14," [TOTAL]" -join ''
+$D14A_Str
+
+#Testing for creating chart
+[void][Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms.DataVisualization")
+
+
+$D14_AG_Chart = New-object System.Windows.Forms.DataVisualization.Charting.Chart
+$D14_AG_Chart.Width = 800
+$D14_AG_Chart.Height = 400
+$D14_AG_Chart.BackColor = [System.Drawing.Color]::White
+		 
+#header 
+[void]$D14_AG_Chart.Titles.Add("Age Group Distribution for Recent 14 Days")
+$D14_AG_Chart.Titles[0].Font = "segoeuilight,20pt"
+$D14_AG_Chart.Titles[0].Alignment = "topLeft"
+$chartarea = New-Object System.Windows.Forms.DataVisualization.Charting.ChartArea
+$chartarea.Name = "ChartArea1"
+$D14_AG_Chart.ChartAreas.Add($chartarea)
+			  
+[void]$D14_AG_Chart.Series.Add("data1")
+		   
+$D14_AG_Chart.Series["data1"].ChartType = [System.Windows.Forms.DataVisualization.Charting.SeriesChartType]::Doughnut
+$D14_AG_Chart.Series["data1"].Points.DataBindXY($D14A_HEADER, $D14A_DATA)
+$D14_AG_Chart.SaveImage("$PWD\D14_AG_Chart.png","png")
+start "$PWD\D14_AG_Chart.png"
